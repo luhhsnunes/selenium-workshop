2c8769ef016b1a8f5eff2b403d46c1963f44880e
bborges <bborges@thoughtworks.com>
2014-09-18 18:30:29 -0300

    Added login test in exercicio2 class

--- test/Exercicio2.java
+++ test/Exercicio2.java
@@ -1,15 +1,11 @@
 /**
  * Created by bborges on 9/18/14.
  */
-import org.hamcrest.MatcherAssert;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
 
 import java.util.concurrent.TimeUnit;
 
@@ -35,13 +31,22 @@ public class Exercicio2 {
     public void createAccount(){
         driver.get("http://www.flipkart.com/");
         driver.findElementByClassName(("signup-link")).click();
-        driver.findElementById("signup-email").sendKeys("1111222345@gmail.com");
+        driver.findElementById("signup-email").sendKeys("pedro@gmail.com");
         driver.findElementById("signup-password").sendKeys("Tartaruga01");
         driver.findElementByName("repeat-password").sendKeys("Tartaruga01");
         driver.findElement(By.cssSelector("input[value = 'Sign Up Now!']")).click();
         boolean result = driver.findElementByClassName("greeting-link").isEnabled();
-        assertThat(result,equalTo(Boolean.TRUE));
+        assertThat(result, equalTo(Boolean.TRUE));
     }
 
+    @Test
+    public void editAccount(){
+        driver.get("http://www.flipkart.com/account");
+        driver.findElementById("login_email_id1").sendKeys("bruno@gmail.com");
+        driver.findElementById("login_password1").sendKeys("Tartaruga01");
+        driver.findElementById("login-cta").click();
+        String result = driver.findElementByClassName("fk-font-verybig").getText();
+        assertThat(result, containsString("Personal Information"));
+    }
 
 }
