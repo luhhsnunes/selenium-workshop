8246b7355a4e909282ed6e71d38e411fb12116a5
Rodrigo Tolledo <rtolled@thoughtworks.com>
2014-09-20 23:18:49 -0300

    Add editPost() and deletePost() tests for Exercicio3

--- test/Exercicio3.java
+++ test/Exercicio3.java
@@ -1,4 +1,5 @@
 import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
 import java.util.concurrent.TimeUnit;
@@ -12,9 +13,9 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 
-// Silly implementation without using Page Object pattern.
+// Silly implementation with lots of code duplication.
 // The implementation below is just for basic learning purposes.
-// For a decent implementation, please see Exercicio4.java
+// For a decent implementation (using Page Object pattern), please see Exercicio4.java
 
 public class Exercicio3 {
 	WebDriver driver;
@@ -22,6 +23,7 @@ public class Exercicio3 {
 	@Before
 	public void setUp() {
 		driver = new FirefoxDriver();
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 	
@@ -48,8 +50,8 @@ public class Exercicio3 {
 		WebElement postBody = driver.findElement(By.id("tinymce"));
 		postBody.sendKeys("This is a description.");
 		driver.switchTo().defaultContent();
-		WebElement title = driver.findElement(By.id("title"));
-		title.sendKeys("My First Post");
+		WebElement postTitle = driver.findElement(By.id("title"));
+		postTitle.sendKeys("My First Post");
 		
 		WebElement publish = driver.findElement(By.id("publish"));
 		publish.click();
@@ -57,5 +59,52 @@ public class Exercicio3 {
 		Boolean titleExists = driver.getPageSource().contains("My First Post");
 		
 		assertThat(titleExists, equalTo(Boolean.TRUE));
+	}
+	
+	@Test
+	public void editPost() {
+		driver.get("http://twseleniumworkshop.wordpress.com/wp-admin");
+		WebElement email = driver.findElement(By.id("user_login"));
+		WebElement password = driver.findElement(By.id("user_pass"));
+		WebElement submit = driver.findElement(By.id("wp-submit"));
+		email.sendKeys("twseleniumworkshop");
+		password.sendKeys("twseleniumworkshop!");
+		submit.click();
+		
+		driver.get("http://twseleniumworkshop.wordpress.com/wp-admin/edit.php");
+		WebElement post = driver.findElement(By.linkText("My First Post"));
+		post.click();
+		driver.switchTo().frame("content_ifr");
+		WebElement newPostBody = driver.findElement(By.id("tinymce"));
+		newPostBody.clear();
+		newPostBody.sendKeys("This is a NEW description.");
+		driver.switchTo().defaultContent();
+		WebElement update = driver.findElement(By.id("publish"));
+		update.click();
+		
+		String updatedPostMessage = driver.findElement(By.cssSelector("#message p")).getText();
+		
+		assertThat(updatedPostMessage, is("Post updated. View post"));
+	}
+	
+	@Test
+	public void deletePost() {
+		driver.get("http://twseleniumworkshop.wordpress.com/wp-admin");
+		WebElement email = driver.findElement(By.id("user_login"));
+		WebElement password = driver.findElement(By.id("user_pass"));
+		WebElement submit = driver.findElement(By.id("wp-submit"));
+		email.sendKeys("twseleniumworkshop");
+		password.sendKeys("twseleniumworkshop!");
+		submit.click();
+		
+		driver.get("http://twseleniumworkshop.wordpress.com/wp-admin/edit.php");
+		WebElement post = driver.findElement(By.linkText("My First Post"));
+		post.click();
+		WebElement deletePost = driver.findElement(By.linkText("Move to Trash"));
+		deletePost.click();
+		
+		String deletedPostMessage = driver.findElement(By.cssSelector("#message p")).getText();
+		
+		assertThat(deletedPostMessage, is("1 post moved to the Trash. Undo"));
 	}
 }
